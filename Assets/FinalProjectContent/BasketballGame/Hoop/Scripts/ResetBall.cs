c857fa4677b3fa4e4f20512282f12e62f0d93946
David Chen <dc30983@uga.edu>
2022-12-04 13:15:52 -0500

    Updated

--- Assets/FinalProjectContent/BasketballGame/Hoop/Scripts/ResetBall.cs
+++ Assets/FinalProjectContent/BasketballGame/Hoop/Scripts/ResetBall.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 
 
-public class Resetball : MonoBehaviour
+public class ResetBall : MonoBehaviour
 {
    public GameObject Object; 
     // Start is called before the first frame update
