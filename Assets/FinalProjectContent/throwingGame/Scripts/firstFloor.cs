57dd37dfabc37a3c653ea78a3cbe3e5ceef2c07f
sum_c <suminyj@gmail.com>
2022-12-07 09:14:59 -0500

    Throwing game edit
    score system, objects added

---
+++ Assets/FinalProjectContent/throwingGame/Scripts/firstFloor.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class firstFloor : MonoBehaviour
+{
+
+    public TMP_Text pntDisplay;
+    private AudioSource audioS;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void addPoints(int n)
+    {
+        Points.totalPoints += n;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "rock")
+        {
+            int newPoint = 3;
+            addPoints(newPoint);
+            pntDisplay.text = newPoint.ToString();
+            GetComponent<Collider>().enabled = false;
+            audioS = GetComponent<AudioSource>();
+            audioS.Play();
+            //Debug.Log(Points.totalPoints);
+        }
+    }
+}
